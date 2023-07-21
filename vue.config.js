efe5332d74a7993aeff27ffad3e5f3bfb105ae61
jack <a0978182932@gmail.com>
2023-07-21 17:27:01 +0800

    modify publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,14 +3,4 @@ module.exports = defineConfig({
   publicPath :process.env.NODE_ENV === 'production'
   ? '/vue-test/'
   : '/'
-  ,
-  devServer:{
-    proxy:{
-      '':{
-        target:'http://127.0.0.1:3000/',
-        ws:true,
-        changeOrigin:true
-      }
-    }
-  }
 })
